8162ac4efd5b05711e65130ac25e975f06b23250
Jarfino HOUNGBADJI <jarfinohoungbadji@gmail.com>
2026-01-26 11:54:36 +0100

    Authentification avec mot de passe et token JWT

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -1,10 +1,20 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 from typing import Annotated
+from datetime import datetime, timedelta, timezone
+
+import jwt
+from jwt.exceptions import InvalidTokenError
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from pydantic import BaseModel
 
 from backend.schema.user import UserCreateDTO, UserReadDTO
 from backend.schema.auth import VerifyOTPRequestUser
 from backend.services.user_service import UserService
 from backend.db.session import SessionDep
+from backend.models.user_entity import User
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -12,6 +22,76 @@ def get_user_service(session: SessionDep) -> UserService:
     return UserService(session)
 
 UserServiceDep = Annotated[UserService, Depends(get_user_service)]
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/token") # Token URL for OAuth2
+
+env_path = Path(__file__).parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], service: UserServiceDep):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = service.get_user_by_email(username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+# def get_current_active_user(
+#     current_user: Annotated[User, Depends(get_current_user)],
+# ):
+#     if current_user.disabled:
+#         raise HTTPException(status_code=400, detail="Inactive user")
+#     return current_user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+@router.post("/token")
+def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    service: UserServiceDep,
+) -> Token:
+    db_user = service.authenticate_user(form_data.username, form_data.password)
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": db_user.email}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
 @router.post("/", response_model=UserReadDTO, status_code=status.HTTP_201_CREATED)
 def create_user(
